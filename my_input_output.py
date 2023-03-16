4565548b080e80ca78855c51b935607d9d3bb0ef
Basabdatta Ray <basabray.juche@gmail.com>
2023-03-16 18:21:47 +0530

    Input Output in Python

---
+++ my_input_output.py
@@ -0,0 +1,7 @@
+def inputOutput():
+    print("Enter a String")
+    str = input()
+    print("Enter an Integer Number")
+    num = int(input())
+    print("Entered String is", str)
+    print("Entered Number is", num)
