0609d44f3278e93151412ee8308a7b3594278714
Basabdatta Ray <basabray.juche@gmail.com>
2023-03-17 23:59:30 +0530

    Factorial Using Recurssion

---
+++ factorial.py
@@ -0,0 +1,5 @@
+def factorial(n):
+	if (n==1 or n==0):
+		return 1
+	else:
+		return (n * factorial(n - 1))
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from my_input_output import inputOutput
+from factorial import factorial
 
 print("Hello World")
 
 inputOutput()
+
+fact = factorial(7)
+print("Factorial of 7 is", fact)
 
